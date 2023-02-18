fef27bad9da75aa56267a6752fde8254e4ae646f
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-02-18 08:08:51 +0600

    added pentagon calculation

--- js/main.js
+++ js/main.js
@@ -84,3 +84,24 @@ document.getElementById("rhombus").addEventListener("click", function (event) {
   //create Result List and append
   createResultList(event, result, serial);
 });
+
+//pentagon calculation
+document.getElementById("pentagon").addEventListener("click", function (event) {
+  const firstInput = getFirstInput(event);
+  const secondInput = getSecondInput(event);
+  //some validation
+  if (isNaN(firstInput) || isNaN(secondInput)) {
+    alert("Please check your input again.Provide a number.Thanks...");
+    return;
+  } else if (firstInput < 1 || secondInput < 1) {
+    alert("Please provide a positive number.");
+    return;
+  }
+  serial++;
+  //main formula
+  const formula = 0.5 * firstInput * secondInput;
+  //get fixed formula ans
+  const result = makeFormulaValuePrecision(formula);
+  //create Result List and append
+  createResultList(event, result, serial);
+});
