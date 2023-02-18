8d6b6fcf53d4ca2238043aa8fc072b42eb3fa448
Md. Abir Mahmud <abirmahmud5665@gmail.com>
2023-02-18 08:07:42 +0600

    Added parallelogram calculation

--- js/main.js
+++ js/main.js
@@ -41,3 +41,25 @@ document
     //create Result List and append
     createResultList(event, result, serial);
   });
+// parallelogram calculation
+document
+  .getElementById("parallelogram")
+  .addEventListener("click", function (event) {
+    const firstInput = getFirstInput(event);
+    const secondInput = getSecondInput(event);
+    //some validation
+    if (isNaN(firstInput) || isNaN(secondInput)) {
+      alert("Please check your input again.Provide a number.Thanks...");
+      return;
+    } else if (firstInput < 1 || secondInput < 1) {
+      alert("Please provide a positive number.");
+      return;
+    }
+    serial++;
+    //main formula
+    const formula = firstInput * secondInput;
+    //get fixed formula ans
+    const result = makeFormulaValuePrecision(formula);
+    //create Result List and append
+    createResultList(event, result, serial);
+  });
